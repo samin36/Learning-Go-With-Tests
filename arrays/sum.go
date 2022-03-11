ea6d295bcabc0df6cc8feb06cc97d11edb65a9c9
Shrey Amin <shrey.amin@stonebranch.com>
2022-03-11 10:47:07 -0500

    Added slices examples

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,25 @@ func Sum(nums []int) (sum int) {
 
 	return
 }
+
+func SumAll(numSlices ...[]int) (sums []int) {
+	for _, nums := range numSlices {
+		sums = append(sums, Sum(nums))
+	}
+
+	return
+}
+
+func SumAllTails(numSlices ...[]int) (tailSums []int) {
+	for _, nums := range numSlices {
+		tail := []int{}
+
+		if len(nums) > 1 {
+			tail = nums[1:]
+		}
+
+		tailSums = append(tailSums, Sum(tail))
+	}
+
+	return
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,7 @@
 package arrays
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -18,5 +19,37 @@ func TestSum(t *testing.T) {
 		got := Sum(numbers)
 		expected := 6
 		assertCorrectSum(t, numbers, got, expected)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sum of 2 slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		expected := []int{3, 9}
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("got %v but expected %v", got, expected)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("got %v but expected %v", got, expected)
+		}
+	}
+
+	t.Run("sum of tails of 2 slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 9}, []int{2, 3, 4})
+		expected := []int{5, 18, 7}
+		checkSums(t, got, expected)
+	})
+
+	t.Run("sum of tails of an empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2, 3})
+		expected := []int{0, 5}
+		checkSums(t, got, expected)
 	})
 }
